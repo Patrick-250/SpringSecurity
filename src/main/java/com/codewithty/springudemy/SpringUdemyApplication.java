e39478cc36593424a5e77820a247e822cb19e85f
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2025-04-01 16:27:13 -0500

    feat:connect to mysql database, hibernate and JPA

--- src/main/java/com/codewithty/springudemy/SpringUdemyApplication.java
+++ src/main/java/com/codewithty/springudemy/SpringUdemyApplication.java
@@ -1,7 +1,9 @@
 package com.codewithty.springudemy;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringUdemyApplication {
@@ -9,5 +11,10 @@ public class SpringUdemyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringUdemyApplication.class, args);
     }
-
+@Bean
+    public CommandLineRunner commandLineRunner () {
+        return runner->{
+            System.out.println("Hello Rumanzi, i swear you got me on this");
+        };
+}
 }
